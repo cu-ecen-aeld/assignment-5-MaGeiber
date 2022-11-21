0cdce82874947ef25517bf64b42b6d7548ab3979
MaGeiber <26193554+MaGeiber@users.noreply.github.com>
2022-11-20 21:03:03 -0800

    Commit Assignment 4 Part 2

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 2d5f29553201948f236fd2b4efe93348a4c04932
+AESD_ASSIGNMENTS_VERSION = d53ffbaaa0f465a4ef215fc87298da0608259f52
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
